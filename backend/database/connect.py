17fc6a395515e125b850e2209872761a85dd9c87
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-06-29 23:30:45 +0530

    set up PostgreSQL database connection

---
+++ backend/database/connect.py
@@ -0,0 +1,16 @@
+import psycopg2
+from config import load_config 
+
+def connect():
+    config = load_config()
+    """ Connect to the PostgreSQL database server """
+    try:
+        # connecting to the PostgreSQL server
+        with psycopg2.connect(**config) as connection:
+            print("Connected to the PostgreSQL server")
+            return connection
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+if __name__ == "__main__":
+    connect()
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+import sys
+
+# add 'database' folder to the system path
+sys.path.insert(0, "./database")
+import connect
 
 app = FastAPI()
+
+# connect to PostgreSQL server
+db_connection = connect.connect()
+db_cursor = db_connection.cursor()
+db_cursor.execute("SELECT version();")
+db_version = db_cursor.fetchone()
+print(db_version)
 
 @app.get("/api/v0/")
 async def root():
