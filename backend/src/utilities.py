8301706e0c29dc3d3c72b3a4c5725864d751a505
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-07-16 22:46:18 +0530

    add Report type validation

---
+++ backend/src/utilities.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+
+class Report(BaseModel):
+    date_time: str
+    title: str
+    location: str
+    directions: str
+    description: str
+    up_votes: int
+    down_votes:int
