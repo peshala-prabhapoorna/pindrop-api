69b30f8512bdda542af110aa84c142c40d7d459b
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-08-13 21:37:51 +0530

    change field type of password

--- backend/database/sql/users.sql
+++ backend/database/sql/users.sql
@@ -5,13 +5,13 @@ CREATE DOMAIN email AS citext
 );
 
 CREATE TABLE IF NOT EXISTS users (
-    id         SERIAL PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL,
-    first_name VARCHAR(32) NOT NULL,
-    last_name  VARCHAR(32) NOT NULL,
-    phone_num  VARCHAR(9) NOT NULL UNIQUE,
-    email      email NOT NULL UNIQUE,
-    password   VARCHAR(64) NOT NULL,
-    deleted_at TIMESTAMP DEFAULT NULL,
-    token      TEXT DEFAULT NULL
+    id              SERIAL PRIMARY KEY,
+    created_at      TIMESTAMP NOT NULL,
+    first_name      VARCHAR(32) NOT NULL,
+    last_name       VARCHAR(32) NOT NULL,
+    phone_num       VARCHAR(9) NOT NULL UNIQUE,
+    email           email NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL,
+    deleted_at      TIMESTAMP DEFAULT NULL,
+    token           TEXT DEFAULT NULL
 );
