3baf39b96cc8e8aed77693a0467e90a6b800f274
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-12-11 23:51:29 +0530

    docs[users]: add docstrings to schemas models
    Add docstrings to models related to the `users` table.

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -4,6 +4,20 @@ from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
+    """
+    `users` table modules are derived from this class.
+
+    `phone_num` is used for 2FA using one-time passwords. Limited number
+    of user accounts can be associated with a single mobile phone.
+    `email` is used to uniquely identify a user.
+
+    Attributes:
+    `first_name` (str): first name of the user
+    `last_name`  (str): last name of the user
+    `phone_num`  (str): mobile phone number of the user
+    `email`      (str): unique email address of the user
+    """
+
     first_name: str
     last_name: str
     phone_num: str
@@ -11,21 +25,61 @@ class UserBase(BaseModel):
 
 
 class UserInDB(UserBase):
+    """
+    Model representing the db record of a user.
+
+    Attributes:
+    `first_name`   (str): first name of the user
+    `last_name`    (str): last name of the user
+    `phone_num`    (str): mobile phone number of the user
+    `email`        (str): unique email address of the user
+    `id`           (int): unique id number of the user
+    `tokens` (List[str]): List of active jwt tokens
+    """
+
     id: int
     tokens: List[str] | None
 
 
-# model to create a user
 class UserIn(UserBase):
+    """
+    Model used to get user input to create a new user.
+
+    Attributes:
+    `first_name` (str): first name of the user
+    `last_name`  (str): last name of the user
+    `phone_num`  (str): mobile phone number of the user
+    `email`      (str): unique email address of the user
+    `password`   (str): password to be used to authenticate the user
+    """
+
     password: str
 
 
-# model to use in responses
 class UserOut(UserBase):
+    """
+    Model used in repsonses sent to the client.
+
+    Attributes:
+    `first_name` (str): first name of the user
+    `last_name`  (str): last name of the user
+    `phone_num`  (str): mobile phone number of the user
+    `email`      (str): unique email address of the user
+    `id`         (int): unique id number of the user
+    """
+
     id: int
 
 
 class UserNameEdit(BaseModel):
+    """
+    Model used to get user input to edit the user's name.
+
+    Attributes:
+    `first_name` (str | None = None): first name of the user
+    `last_name`  (str | None = None): last name of the user
+    """
+
     first_name: str | None = None
     last_name: str | None = None
 
